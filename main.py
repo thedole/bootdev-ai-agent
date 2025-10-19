7b30a061527810982b3c145d80b5b37c7c815636
thedole <thedole@gmail.com>
2025-10-20 01:08:57 +0200

    added verbose mode, and made the content ready to contain a list of prompts

--- main.py
+++ main.py
@@ -2,17 +2,31 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+# print(sys.argv)
 if len(sys.argv) < 2:
     print("Usage: uv run main.py <your prompt here>")
     sys.exit(1)
+is_verbose = False
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    is_verbose = True
 
-prompt = sys.argv[1]
+user_prompt = sys.argv[1]
+if is_verbose:
+    print(f"User prompt: '{user_prompt}'")
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key= api_key)
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", 
+    contents=messages
+    )
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if is_verbose:
+  print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+  print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
