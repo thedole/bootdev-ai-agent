e5e8b237eeec579be47c73f235f6aff6b55aefec
thedole <thedole@gmail.com>
2025-10-20 00:45:17 +0200

    moved the prompt to a command line argument

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+
+if len(sys.argv) < 2:
+    print("Usage: uv run main.py <your prompt here>")
+    sys.exit(1)
+
+prompt = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key= api_key)
-
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
 print(response.text)
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-
-# def main():
-#     print("Hello from bootdev-ai-agent!")
-
-
-# if __name__ == "__main__":
-#     main()
